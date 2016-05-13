7eb59267e06eb50b66f5035e57c578e9270b645b
Darian Rosebrook <d.evyn.webdev@gmail.com>
2016-05-13 07:18:01 -0700

    why do we have to write messages all the time

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -41,7 +41,7 @@ function getPageScroll() {
     yScroll = window.pageYOffset;
   } else if (document.documentElement && document.documentElement.scrollTop) {
     yScroll = document.documentElement.scrollTop;
-  } else if (document.body) {
+  } else if (document.body) { //last resort
     yScroll = document.body.scrollTop;
   }
   return yScroll;
