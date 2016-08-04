b5c07318998c88be9e0109fa268412be67959f23
darian <git@darianrosebrook.com>
2016-08-03 21:41:40 -0700

    merged the online changes and started working on the json ajax call js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -142,22 +142,20 @@ var loadMore = byId('load-more');
 function loadJSON(callback) {
    var xobj = new XMLHttpRequest();
        xobj.overrideMimeType("application/json");
-   xobj.open('GET', '../scripts/posts.json', true); // Replace 'my_data' with the path to your file
+   xobj.open('GET', 'https://darianrosebrook.com/scripts/posts.json', true); // Replace 'my_data' with the path to your file
    xobj.onreadystatechange = function () {
          if (xobj.readyState == 4 && xobj.status == "200") {
            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+           console.log(xobj.responseText);
            callback(xobj.responseText);
          }
+   console.log(xobj);
    };
    xobj.send(null);
 }
-
-if (byId('post-list')) {
-  loadMore.addEventListener('click', function init() {
+function init() {
  loadJSON(function(response) {
   // Parse JSON string into object
     var actual_JSON = JSON.parse(response);
-    console.log(actual_JSON);
  });
-})
 }
