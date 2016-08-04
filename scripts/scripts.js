b048673a55356bea11f3895f51b898ccae2c99b8
darian <git@darianrosebrook.com>
2016-08-03 23:56:52 -0700

    added for loop that creates the html markup for each of the new files. need to create conditionals that filter a little better.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -139,23 +139,88 @@ if(parentProcess) {
 }
 // Load more posts functionality ajax request
 var loadMore = byId('load-more');
-function loadJSON(callback) {
-   var xobj = new XMLHttpRequest();
-       xobj.overrideMimeType("application/json");
-   xobj.open('GET', 'https://darianrosebrook.com/scripts/posts.json', true); // Replace 'my_data' with the path to your file
-   xobj.onreadystatechange = function () {
-         if (xobj.readyState == 4 && xobj.status == "200") {
-           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-           console.log(xobj.responseText);
-           callback(xobj.responseText);
-         }
-   console.log(xobj);
-   };
-   xobj.send(null);
-}
+  function loadJSON(callback) {
+    var jsonStuff = new XMLHttpRequest();
+    jsonStuff.overrideMimeType("application/json");
+    jsonStuff.open('GET', 'https://darianrosebrook.com/scripts/posts.json', true);
+    jsonStuff.onreadystatechange = function () {
+      if (jsonStuff.readyState == 4 && jsonStuff.status == "200") {
+        callback(jsonStuff.responseText);
+      }
+      console.log(jsonStuff);
+    };
+    jsonStuff.send(null);
+  }
 function init() {
  loadJSON(function(response) {
   // Parse JSON string into object
     var actual_JSON = JSON.parse(response);
+
  });
 }
+var list = byId('list');
+var oldContent = list.innerHTML;
+var xobj ={
+  "posts": [
+
+      {
+        "title": "Burn the Boats&#58; An Update On My Current 2016 Goals",
+        "url": "/article/Update-On-2016-Goals",
+        "date": "Jul 15, 2016",
+        "img": "assets/torch.jpg",
+        "content": "What I love about phrases like these is that though they are removed from any part of what we do now in our time, their message is reused and repurposed into something that fits our current situations.",
+        "class": "type-life",
+        "type": "Life Stuff"
+      } ,
+
+      {
+        "title": "Handling Expectational Debt And Falling Behind.",
+        "url": "/article/handling-expectational-debt-and-falling-behind",
+        "date": "May 20, 2016",
+        "img": "assets/expectational-debt.jpg",
+        "content": "Why are we all given the same 24 hours? A lot of people seem to get more done than I can, and yet no one has more time during the day than others. Even since I started working 6 years ago, I have not earned any extra time in my life. Why do I feel more exhausted than my earlier self?",
+        "class": "type-life",
+        "type": "Life-Hacks"
+      } ,
+
+      {
+        "title": "A Work Flow Process for Creating Better Websites",
+        "url": "/article/Web-Design-Work-Flow-for-2016",
+        "date": "May 12, 2016",
+        "img": "assets/office.jpg",
+        "content": "Designing websites is so much more than throwing together some layout and fonts. The process dives much deeper than that. What web design has become is an iterative process of discovery and problem solving.",
+        "class": "type-web",
+        "type": "Web Design"
+      } ,
+
+      {
+        "title": "Branding and Logo Design Work Flow for 2016",
+        "url": "/article/Branding-and-Logo-Design-Work-Flow-for-2016",
+        "date": "May 20, 2016",
+        "img": "assets/rocks-hands.jpg",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a ...",
+        "class": "type-brand",
+        "type": "Branding"
+      } ,
+
+      {
+        "title": "How To Be Like Darian",
+        "url": "/article/How-To-Be-like-Darian",
+        "date": "Jan 1, 2016",
+        "img": "assets/notebook.jpg",
+        "content": "Be Awesome. Nuff Said.",
+        "class": "type-life",
+        "type": "Life Stuff"
+      }
+
+
+  ]
+}
+;
+var newContent = '';
+for (var i = 0; i < xobj.posts.length; i++) {
+  newContent += '<article class=\"list-item\">\n\t<div class=\"circle\">';
+  newContent += '<img src=\"https://darianrosebrook.com' + xobj.posts[i].img + '\" alt=\"' + xobj.posts[i].title + '\" /></div>\n';
+  newContent += '<div class=\"artcont\">\n\t<h2><a href=\"' + xobj.posts[i].url + '\">' + xobj.posts[i].title + '</a></h2>\n\t';
+  newContent += '<p><small>' + xobj.posts[i].when + '<em class=\"' + xobj.posts[i].class + " \>" + xobj.posts[i].type + '</em></small>\n';
+  newContent += '<small class=\"metaDesc\">&ldquo;' + xobj.posts[i].metaDesc + '&rdquo;</small></p></div></article>'; }
