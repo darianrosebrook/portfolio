d4c2883586efa99e88ae2c4b5977fa9d46fcf246
d-evyn <hello@darianrosebrook.com>
2022-11-29 00:00:06 -0800

    removed most of the boilerplate

---
+++ src/lib/components/OG.svelte
@@ -0,0 +1,20 @@
+
+<script>
+	export let message = 'Hello, world!';
+</script>
+
+<div style="color: red">{message}</div>
+
+<style>
+	div {
+		font-family: 'Noto Sans';
+		display: flex;
+		flex-direction: column;
+		font-size: 50px;
+		background-color: lightblue;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import fs from 'fs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), rawFonts(['.ttf'])],
+  optimizeDeps: {
+    exclude: ['@vercel/og']
+  },
   define: {
     'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
   }
 };
+function rawFonts(ext) {
+  return {
+    name: 'vite-plugin-raw-fonts',
+    resolveId(id) {
+      return ext.some((e) => id.endsWith(e)) ? id : null;
+    },
+    transform(code, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return { code: `export default ${JSON.stringify(buffer)}`, map: null };
+      }
+    }
+  };
+}
 
 export default config;
