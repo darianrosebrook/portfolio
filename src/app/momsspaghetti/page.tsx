a188cc5fe8e920ae21f51ac589103accb018ea98
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-01 16:26:01 -0800

    fix rendered by removing session

--- src/app/momsspaghetti/page.tsx
+++ src/app/momsspaghetti/page.tsx
@@ -1,38 +1,36 @@
-'use client'
+"use client";
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-import type { Session } from '@supabase/auth-helpers-nextjs'
+import type { Session } from "@supabase/auth-helpers-nextjs";
 
-export default function LoginForm({ session }: { session: Session | null }) { 
-  const router = useRouter()
-  const supabase = createClientComponentClient()
- 
+export default function AuthButton() {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: "google",
       options: {
         redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}/auth/callback`,
       },
-    })
-    router.refresh()
-  }
+    });
+    router.refresh();
+  };
 
   const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
+    await supabase.auth.signOut();
+    router.refresh();
+  };
 
   // for the `session` to be available on first SSR render, it must be
   // fetched in a Server Component and passed down as a prop
-  return session ? (
-    <button onClick={handleSignOut}>Sign out</button>
-  ) : (
-    <>  
+  return (
+    <>
       <button onClick={handleSignIn}>Sign in</button>
+      <button onClick={handleSignOut}>Sign out</button>
     </>
-  )
+  );
 }
