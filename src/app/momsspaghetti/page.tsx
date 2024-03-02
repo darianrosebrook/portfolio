e3ac70142313e0bc10d3f5b7895f257eaf2717be
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-01 17:05:44 -0800

    updated urls for login

--- src/app/momsspaghetti/page.tsx
+++ src/app/momsspaghetti/page.tsx
@@ -1,36 +1,10 @@
-"use client";
+import { login, signOut } from './actions'
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-
-import type { Session } from "@supabase/auth-helpers-nextjs";
-
-export default function AuthButton() {
-  const router = useRouter();
-  const supabase = createClientComponentClient();
-
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}/auth/callback`,
-      },
-    });
-    router.refresh();
-  };
-
-  const handleSignOut = async () => {
-    await supabase.auth.signOut();
-    router.refresh();
-  };
-
-  // for the `session` to be available on first SSR render, it must be
-  // fetched in a Server Component and passed down as a prop
+export default function LoginPage() {
   return (
-    <>
-      <button onClick={handleSignIn}>Sign in</button>
-      <button onClick={handleSignOut}>Sign out</button>
-    </>
-  );
+    <form> 
+      <button formAction={login}>Log in</button> 
+      <button formAction={signOut}>Sign out</button>
+    </form>
+  )
 }
