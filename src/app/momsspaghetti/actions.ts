e9c6c325315faa531a9687b56dee0c69511dd79f
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-01 17:05:44 -0800

    updated urls for login

--- src/app/momsspaghetti/actions.ts
+++ src/app/momsspaghetti/actions.ts
@@ -1,29 +1,32 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
 import { createClient } from "../../../utils/supabase/server";
 
 export async function login() {
   const client = createClient();
-  console.log('clicking login button');
-  const { error } = await client.auth.signInWithOAuth({
-    provider: 'google',
-    options: { redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}/auth/callback` },
+  console.log("clicking login button");
+  const { data, error } = await client.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}/auth/callback`,
+    },
   });
-  if (error) {
-    console.error('Error logging in:', error);
+  if (data.url) {
+    console.log("data:", data);
+    redirect(data.url);
+  } else if (error) {
+    console.error("Error logging in:", error);
   } else {
-    revalidatePath('/private');
+    redirect("/");
   }
 }
 export async function signOut() {
   const client = createClient();
   const { error } = await client.auth.signOut();
   if (error) {
-    console.error('Error logging out:', error);
+    console.error("Error logging out:", error);
   } else {
-    revalidatePath('/');
+    redirect("/");
   }
 }
