fd10d24430098eb89b50913e22d749f233f159c3
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-12 18:51:23 -0700

    ha

--- app/momsspaghetti/actions.ts
+++ app/momsspaghetti/actions.ts
@@ -2,13 +2,23 @@
 
 import { redirect } from "next/navigation";
 import { createClient } from "../../utils/supabase/server";
-
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
 export async function login() {
   const client = createClient(); 
   const { data, error } = await client.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}/auth/callback`,
+      redirectTo: getURL(),
     },
   });
   if (data.url) { 
