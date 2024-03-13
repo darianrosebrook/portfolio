f71595751cf2a786849885feea10e42c182ca8ea
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-12 18:30:45 -0700

    worked uploads for images

--- components/tiptap/tiptap.tsx
+++ components/tiptap/tiptap.tsx
@@ -1,22 +1,63 @@
 "use client";
 
 import { useEditor, EditorContent, JSONContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import styles from "./tiptap.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import Avatar from "@/components/avatar/avatar";
 import { Database } from "app/types/supabase";
+import styles from "./tiptap.module.css";
+import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+
+// upload image
+import { upload } from "@/utils/upload";
 
 const Tiptap = ({
   article,
 }: {
-  article: Database["public"]["Tables"]["articles"]["Row"];
+  article?: Database["public"]["Tables"]["articles"]["Row"];
 }) => {
-  const content = article.articleBody as JSONContent;
+  const images = new Map<string, string>();
+  const content = article?.articleBody as JSONContent | undefined;
   const editor = useEditor({
     content: content,
-    extensions: [StarterKit],
+    extensions: [StarterKit, Image],
+    editorProps: {
+      handleDrop: function (view, event, slice, moved) {
+        if (
+          !moved &&
+          event.dataTransfer &&
+          event.dataTransfer.files &&
+          event.dataTransfer.files[0]
+        ) {
+          const file = event.dataTransfer.files[0];
+          const newImage = document.createElement("img");
+          newImage.src = URL.createObjectURL(file);
+          newImage.onload = async () => {
+            const path = await upload({
+              file: {
+                type: "image",
+                media: {
+                  name: file.name,
+                  data: file,
+                },
+              },
+              bucket: "article-images",
+            });
+            images.set(file.name, path);
+
+            const { schema } = view.state;
+            const coordinates = view.posAtCoords({
+              left: event.clientX,
+              top: event.clientY,
+            });
+            const node = schema.nodes.image.create({ src: path }); // creates the image element
+            const transaction = view.state.tr.insert(coordinates.pos, node); // places it in the correct position
+            return view.dispatch(transaction);
+          };
+        } else {
+          return false;
+        }
+        
+      },
+    },
   });
   return (
     <>
