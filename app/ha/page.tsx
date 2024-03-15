12d6ca5ce690a2a2c9e1d7925d28b1b429795455
Robot Darian <drosebrookrobinson@ebay.com>
2024-03-14 17:35:22 -0700

    user aware navigation finished

---
+++ app/ha/page.tsx
@@ -0,0 +1,5 @@
+import LoginButton from "@/components/header/login-button";
+
+export default function Page() {
+  return <LoginButton />;
+}
--- app/momsspaghetti/page.tsx
+++
@@ -1,9 +0,0 @@
-import { login, } from './actions'
-
-export default function Page() {
-  return (
-    <form> 
-      <button formAction={login}>Log in</button>
-    </form>
-  )
-}
