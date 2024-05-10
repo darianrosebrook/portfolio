f585a875df71c91039033f7898eeff0f57af63e6
darianrosebrook <hello@darianrosebrook.com>
2024-05-09 22:34:21 -0700

    renamed components folder to fix prev rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
-import OutlineBox from "@/component/OutlineBox";
+import OutlineBox from "@/components/OutlineBox";
 export default function Home() {
   return (
     <>
