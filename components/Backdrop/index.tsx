8fbc60de108ac869008070422840dce2a1ff130b
darianrosebrook <hello@darianrosebrook.com>
2024-06-24 22:40:49 -0700

    add 3d back

--- components/Backdrop/index.tsx
+++ components/Backdrop/index.tsx
@@ -1,8 +1,24 @@
+'use client'
+import { useRef } from 'react';
+import Spline from '@splinetool/react-spline';
+
 export default function Backdrop() {
+  const spline = useRef();
+
+  function onLoad(splineApp) {
+    // save the app in a ref for later use
+    spline.current = splineApp;
+  }
+
+  function triggerAnimation() {
+    if (spline.current) {
+      console.log('triggering animation');
+
+    }
+  }
+
   return (
-    <div className="backdropContainer">
-      <div className="backdrop" />;
-      <div className="cover" />
-    </div>
+    <Spline onMouseHover={triggerAnimation} scene="https://prod.spline.design/nxJyxVaUbfcOw6ST/scene.splinecode" />
+
   );
 }
