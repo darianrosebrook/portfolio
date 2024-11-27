99bddb610e84c0466e83be53196dbb8997a4f3c2
darianrosebrook <hello@darianrosebrook.com>
2024-11-26 20:25:23 -0800

    updated svg

--- app/heroes/swatches.tsx
+++ app/heroes/swatches.tsx
@@ -212,8 +212,70 @@ const Swatches = () => {
         <div
             className={`${Style.gridContainer}`} 
         >
-            <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}>
-                {/* SVG definitions */}
+            <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}> 
+            <symbol id="isometric" viewBox="0 0 142 265">
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M2.80566 7.28925C4.01865 6.50071 5.74057 6.56517 7.65054 7.66789L17.6264 1.90832C15.7165 0.805593 13.9945 0.741133 12.7815 1.52967L2.80566 7.28925Z"
+                        strokeWidth="1"
+                        strokeLinejoin="round"
+                    />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M125.145 75.5027C128.817 77.6232 131.795 82.7807 131.795 87.0219L141.771 81.2623C141.771 77.0211 138.793 71.8636 135.12 69.7432L125.145 75.5027Z"
+                        strokeWidth="1"
+                        strokeLinejoin="round"
+                    />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M131.795 257.25C131.795 259.285 131.109 260.74 129.989 261.468L139.965 255.708C141.085 254.981 141.771 253.526 141.771 251.49L131.795 257.25Z"
+                        strokeWidth="1"
+                        strokeLinejoin="round"
+                    />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M7.65137 7.66778L125.145 75.5028L135.121 69.7432L17.6273 1.9082L7.65137 7.66778Z"
+                        strokeWidth="1"
+                        strokeLinejoin="round"
+                    />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M131.795 87.0223V257.25L141.771 251.49V81.2627L131.795 87.0223Z"
+                        strokeWidth="1"
+                        strokeLinejoin="round"
+                    />
+                    <g clipPath="url(#clip0_142_21895)">
+                        <rect
+                            width="151.029"
+                            height="185.586"
+                            rx="7.67944"
+                            transform="matrix(0.866025 0.5 0 1 1 3.82812)"
+                        />
+                    </g>
+                    <rect
+                        width="151.029"
+                        height="185.586"
+                        rx="7.67944"
+                        transform="matrix(0.866025 0.5 0 1 1 3.82812)"
+                        strokeWidth="1"
+                    />
+
+                    <defs>
+                        <clipPath id="clip0_142_21895">
+                            <rect
+                                width="151.029"
+                                height="185.586"
+                                rx="7.67944"
+                                transform="matrix(0.866025 0.5 0 1 1 3.82812)"
+                            />
+                        </clipPath>
+                    </defs>
+                </symbol> 
             </svg>
             <div className={`${Style.colorSwatchContainer} ${Style.gridContent}`} ref={gridRef}>
                 {colors.map((swatch, i) => {
