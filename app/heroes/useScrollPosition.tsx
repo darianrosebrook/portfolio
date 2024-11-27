34b9a5d16f5a5c92375225f6e656cc08a122ff3a
darianrosebrook <hello@darianrosebrook.com>
2024-11-26 19:43:20 -0800

    adding limits to scroll, and fixing svg size

--- app/heroes/useScrollPosition.tsx
+++ app/heroes/useScrollPosition.tsx
@@ -11,8 +11,8 @@ export const useScrollPosition = (elementRef) => {
             const viewportMiddlePosition = window.visualViewport.offsetTop + window.visualViewport.height / 2; // Middle of the viewport
             const elementMiddlePosition = rect.top + rect.height / 2; // Middle of the element
 
-            const percentFromMiddle =  (elementMiddlePosition - viewportMiddlePosition) / window.visualViewport.height;
-
+            const position =  (elementMiddlePosition - viewportMiddlePosition) / window.visualViewport.height 
+            const percentFromMiddle = Math.min(1, Math.max(-1, position)); // Clamp between -1 and 1
             setRelativePosition({
                 percentFromMiddle,
             });
