de964e69f2ede6894058b1f6112721848c631f6d
darianrosebrook <hello@darianrosebrook.com>
2024-11-26 21:17:13 -0800

    update to ticker function to better handle ref

--- app/heroes/blueprints.tsx
+++ app/heroes/blueprints.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Styles from './blueprints.module.scss';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -17,47 +17,46 @@ const Blueprints: React.FC = () => {
 
   const numRows = 9;
   const numCols = 12;
-  const middleRowIndex = Math.floor(numRows / 2);
- 
+  const middleRowIndex = Math.floor(numRows / 2); 
+
   const [svgs, setSvgs] = useState<string[]>([]);
   const percentInView = useScrollPosition(gridRef);
-
-  const rows = useMemo(() => {
-    return gridRef.current ? Array.from(gridRef.current.children) as HTMLElement[] : [];
-  }, [ ]);
-
-  const handleMouseMove = useCallback((event: MouseEvent) => {
-    mousePos.current.x = event.clientX;
-    mousePos.current.y = event.clientY;
-  }, []);
-
-  const tickerFunction = useCallback(() => { 
-
-    const normalizedMouseX = (mousePos.current.x / winsize.width) * 2 - 1;
-    const targetTranslateX = normalizedMouseX * 12 * (winsize.width / 80);
-
-    rows.forEach((row, index) => {
-      const distanceFromMiddle = Math.abs(index - middleRowIndex);
-      const factor = 1 - 0.2 * distanceFromMiddle;
-      const targetX = targetTranslateX * factor;
-
-      gsap.to(row, {
-        x: targetX,
-        duration: 1.5,
-        ease: 'power2.out',
-      });
-    });
-  }, [ rows,   winsize.width, middleRowIndex]);
 
   useGSAP(() => {
     if (!gridRef.current) return;
     if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) return;
 
+    const rows = Array.from(gridRef.current.children) as HTMLElement[];
+
+    const handleMouseMove = (event: MouseEvent) => {
+      mousePos.current.x = event.clientX;
+      mousePos.current.y = event.clientY;
+    };
+    let distanceModifier = 0.2;
     if (window.innerWidth > 768) {
       window.addEventListener('mousemove', handleMouseMove);
     } else {
-      mousePos.current.x = percentInView * winsize.width;
+      mousePos.current.x = (percentInView + 1) / 3 * winsize.width;  
+      distanceModifier = .4
     }
+
+    const tickerFunction = () => { 
+
+      const normalizedMouseX = (mousePos.current.x / winsize.width) * 2 - 1;
+      const targetTranslateX = normalizedMouseX * 12 * (winsize.width / 80);
+
+      rows.forEach((row, index) => {
+        const distanceFromMiddle = Math.abs(index - middleRowIndex);
+        const factor = 1 - distanceModifier * distanceFromMiddle;
+        const targetX = targetTranslateX * factor;
+
+        gsap.to(row, {
+          x: targetX,
+          duration: 1.5,
+          ease: 'power2.out',
+        });
+      });
+    };
 
     gsap.ticker.add(tickerFunction);
 
@@ -65,7 +64,7 @@ const Blueprints: React.FC = () => {
       window.removeEventListener('mousemove', handleMouseMove);
       gsap.ticker.remove(tickerFunction);
     };
-  }, [handleMouseMove, tickerFunction, percentInView, winsize.width]);
+  }, [ winsize.width, percentInView, gridRef]);
 
   useEffect(() => {
     const dsSprite = document.getElementById('DSSPRITE') as HTMLDivElement;
