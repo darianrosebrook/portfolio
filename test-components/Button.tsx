c646c5b94511a35a6eff519b14ecf7160bf416e1
darianrosebrook <hello@darianrosebrook.com>
2025-11-25 06:22:35 -0800

    update the tokens format to full w3c standards

---
+++ test-components/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button({ label, variant }: { label: string; variant?: "primary" | "secondary" }) {
+  return (
+    <button className="btn btn-custom">
+      {label}
+    </button>
+  );
+}
